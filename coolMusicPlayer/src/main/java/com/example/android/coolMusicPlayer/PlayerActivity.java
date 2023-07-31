ea2899f6f34281aea8172cf508914365b83f7516
Sory <sory.dakota@gmail.com>
2023-07-31 17:31:48 +0200

    Added resources for player controls. play when sleect item, Stop also works.

--- coolMusicPlayer/src/main/java/com/example/android/coolMusicPlayer/PlayerActivity.java
+++ coolMusicPlayer/src/main/java/com/example/android/coolMusicPlayer/PlayerActivity.java
@@ -2,10 +2,12 @@ package com.example.android.coolMusicPlayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,7 +21,7 @@ public class PlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
-        ArrayList<Song> songs = new ArrayList<Song>();
+        ArrayList<Song> songs = new ArrayList<>();
         songs.add(new Song("Flowers","Miley Cyrus"));
         songs.add(new Song("Karma", "Taylor Swift"));
         songs.add(new Song("Eyes closed", "Ed Sheeran"));
@@ -31,29 +33,47 @@ public class PlayerActivity extends AppCompatActivity {
 
 
         SongsAdapter itemsAdapter = new SongsAdapter(this, songs);
-        ListView listView = (ListView) findViewById(R.id.list);
+        ListView listView = findViewById(R.id.list);
         listView.setAdapter(itemsAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
 
                 Song song = (Song) parent.getAdapter().getItem(position);
 
+                TextView title = findViewById(R.id.current_song_text_view);
+                title.setText(song.getSong());
+                TextView author = findViewById(R.id.current_author_text_view);
+                author.setText(song.getAuthor());
+
                 Toast.makeText(getApplicationContext(),
                                 "Now playing " + song.getSong() + " by "+ song.getAuthor(),
                                 Toast.LENGTH_LONG)
                         .show();
 
-                TextView title = (TextView) findViewById(R.id.current_song_text_view);
-                title.setText(song.getSong());
-                TextView author = (TextView) findViewById(R.id.current_author_text_view);
-                author.setText(song.getAuthor());
             }
         });
 
+        ImageButton stopButton = findViewById(R.id.stop_button);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
 
+                TextView title = findViewById(R.id.current_song_text_view);
+                title.setText("");
+                TextView author = findViewById(R.id.current_author_text_view);
+                author.setText("");
+
+                ListView listView = findViewById(R.id.list);
+            }
+        });
+
+        //TODO: click on play -> trigger onclick on current selected item in list
+
+        //TODO: next button -> move to next
 
     }
 }
